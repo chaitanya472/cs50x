bfa38b518b66f6fc241e5ab6f2246d0436d63473
Chaitanya Goli <goli.chinnu@gmail.com>
2020-07-07 21:33:51 -0700

    pong1 - enlarges printed text with good resolution

--- pong/main.lua
+++ pong/main.lua
@@ -1,15 +1,35 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         vsync = true,
         resizable = false
     })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
 
 function love.draw()
-    love.graphics.printf("Hello Pong!", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
+    push:apply('start')
+
+    love.graphics.printf(
+        'Hello Pong!',
+        0,
+        VIRTUAL_HEIGHT / 2 - 6,
+        VIRTUAL_WIDTH,
+        'center')
+
+    push:apply("end")
 end
