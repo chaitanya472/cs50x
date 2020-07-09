d8c8dc1d1d24df9144f41e7e0f87e2c38ebb2dd2
Chaitanya Goli <goli.chinnu@gmail.com>
2020-07-09 11:26:37 -0700

    Pong4 - Clamps the paddles when they reach the edge to stop them from moving past the edge - Makes the ball move when gameState is play and return to is original position when gameState is start - Allows for user to swap gameStates from play to start and vise versa by pressing the enter or return key - Print out hello the the gameState to tell the user what gameState they are curently at

--- pong/main.lua
+++ pong/main.lua
@@ -19,13 +19,17 @@ function love.load()
     -- Sets a font to the variable scoreFont from file font.TTf with a size of 32
     scoreFont = love.graphics.newFont('font.ttf', 32)
 
-    -- Sets up the score for both players
+    -- Sets up the players scores and the y value for their paddles
     player1Score = 0
     player2Score = 0
-
-    -- Sets up the y value for each players paddles
     player1Y = 30
     player2Y = VIRTUAL_HEIGHT - 40
+
+
+    ballStartUp()
+
+    --Sets up the state for th gam
+    gameState = 'start'
 
     -- Sets up the screeen and its dimensions
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -35,28 +39,54 @@ function love.load()
     })
 end
 
+-- Sets the balls x and y and velocity values to its starting values
+function ballStartUp()
+    math.randomseed(os.time())
+    ballX = VIRTUAL_WIDTH / 2 - 2
+    ballY = VIRTUAL_HEIGHT / 2 - 2
+    ballDX = math.random(2) == 1 and -100 or 100
+    ballDY = math.random(-50, 50)
+end
+
 -- Updates the code regardless of the frame rate
 function love.update(dt)
 
     -- if player 1 is pressing w paddle will move up and s will make it move down
+    -- clamps th y value at the edges for paddle 1
     if love.keyboard.isDown('w') then
-        player1Y = player1Y - PADDLE_SPEED * dt
+        player1Y = math.max(0, player1Y - PADDLE_SPEED * dt)
     elseif love.keyboard.isDown('s') then
-        player1Y = player1Y + PADDLE_SPEED * dt
+        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
     end
 
     -- if player 2 is pressing up paddle will move up and down will make it move down
+    -- clamps the y value at the edges for paddle 2
     if love.keyboard.isDown('up') then
-        player2Y = player2Y - PADDLE_SPEED * dt
+        player2Y = math.max(0, player2Y - PADDLE_SPEED * dt)
     elseif love.keyboard.isDown("down") then
-        player2Y = player2Y + PADDLE_SPEED * dt
+        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+    end
+
+    if gameState == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
     end
 end
 
--- When user presses the escape key the screen closes and the code stops
+-- Updates the code when a specific key is pressed
 function love.keypressed(key)
+    -- When user presses the escape key the screen closes and the code stops
     if key == 'escape' then
         love.event.quit()
+
+    -- When the user presses the enter key the game will swap its gameState
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        elseif gameState == 'play' then
+            gameState = 'start'
+            ballStartUp()
+        end
     end
 end
 
@@ -71,8 +101,12 @@ function love.draw()
     --Sets font to small font
     love.graphics.setFont(smallFont)
 
-    -- Prints hello above the ball using the new font
-    love.graphics.printf( "Hello Pong!", 0, 20, VIRTUAL_WIDTH, 'center')
+    -- Prints hello and the current state above the ball using the new font
+    if gameState == 'start' then
+        love.graphics.printf("Hello Start State!", 0, 20, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'play' then
+        love.graphics.printf("Hello Play State!", 0, 20, VIRTUAL_WIDTH, 'center')
+    end
 
     -- Sets font to score font
     love.graphics.setFont(scoreFont)
@@ -83,11 +117,11 @@ function love.draw()
 
 
     -- Makes the ball at the center of the screeen
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
+    love.graphics.rectangle('fill', ballX, ballY, 5, 5)
 
     -- Makes the two paddle at opposite sides of the screen
     love.graphics.rectangle('fill', 5, player1Y, 5, 20)
     love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
-    
+
     push:apply("end")
 end
