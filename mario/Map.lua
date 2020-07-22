fe2d423565c9a8639a96bffee3b3cad0a89a3fb4
Chaitanya Goli <goli.chinnu@gmail.com>
2020-07-22 10:55:12 -0700

    mario3 - Now randomly generates clouds, bushes, jump blocks, mushrooms, and gaps in the map

--- mario/Map.lua
+++ mario/Map.lua
@@ -3,6 +3,21 @@ Map = Class{}
 TILE_BRICK = 1
 TILE_EMPTY = 4
 
+-- cloud tiles
+CLOUD_LEFT = 6
+CLOUD_RIGHT = 7
+
+-- bush tiles
+BUSH_LEFT = 2
+BUSH_RIGHT = 3
+
+-- mushroom tiles
+MUSHROOM_TOP = 10
+MUSHROOM_BOTTOM = 11
+
+-- jump block
+JUMP_BLOCK = 5
+
 local SCROLL_SPEED = 62
 
 -- Assigns all of the values of class map to the object
@@ -22,7 +37,7 @@ function Map:init()
 
    -- Sets up the velocity for the camera will move
    self.camX = 0
-   self.camY = 0
+   self.camY = -3
 
    -- Assigns the spreadsheet as quads to table tileSprites
    self.tileSprites = generateQuads(self.spritesheet, self.tileWidth, self.tileHeight)
@@ -38,12 +53,62 @@ function Map:init()
         end
     end
 
-    -- starts halfway down the map and populates it with bricks
-    for y = self.mapHeight / 2, self.mapHeight do
-        for x = 1, self.mapWidth do
-            self:setTile(x, y, TILE_BRICK)
+    local x = 1
+    while x < self.mapWidth do
+
+        -- 1/20 chance to generate clouds when at least 2 spaces from the edge
+        if x < self.mapWidth - 2 then
+            if math.random(20) == 1 then
+
+                -- Makes sure clouds won't generate on top of other tiles
+                local cloudStart = math.random(self.mapHeight / 2 - 6)
+                self:setTile(x, cloudStart, CLOUD_LEFT)
+                self:setTile(x + 1, cloudStart, CLOUD_RIGHT)
+            end
         end
-    end
+
+        -- 1/20 chance to generate a mushroom
+        if math.random(20) == 1 then
+            self:setTile(x, self.mapHeight / 2 - 2, MUSHROOM_TOP)
+            self:setTile(x, self.mapHeight / 2 - 1, MUSHROOM_BOTTOM)
+
+            -- Under the mushroom generates a colomn of bricks to the edge of the map
+            self:brickColomn(x)
+
+            -- increments x to push the loop to generate the next colomn of the map
+            x = x + 1
+
+        -- 1/10 chance to generate a bush away from the edge
+        elseif math.random(10) == 1 and x < self.mapWidth - 3 then
+            local bushLevel = self.mapHeight / 2 - 1 
+
+            -- Generates left bush with bricks underneath
+            self:setTile(x, bushLevel, BUSH_LEFT)
+            self:brickColomn(x)
+            x = x + 1
+            self:setTile(x, bushLevel, BUSH_RIGHT)
+            self:brickColomn(x)
+            x = x + 1
+        
+        -- 1/10 chance to generate a gap
+        elseif math.random(10) ~= 1 then
+
+            -- Generates a colomn of bricks to the bottom
+            self:brickColomn(x)
+            
+            -- 1/15 chance to generate a jump block
+            if math.random(15) == 1 then 
+                self:setTile(x, self.mapHeight / 2 -4, JUMP_BLOCK)
+            end
+
+            -- Goes to the next vertical line
+            x = x + 1
+        else
+
+            -- Skips two lines making a gap
+            x = x + 2
+        end
+    end              
 end
 
 -- Sets given tile to table tile
@@ -56,6 +121,12 @@ function Map:getTile(x, y)
     return self.tiles[(y - 1) * self.mapWidth + x]
 end
 
+-- Makes a colomn of bricks to the bottom edge of the map
+function Map:brickColomn(x)
+    for y = self.mapHeight / 2, self.mapHeight do
+        self:setTile(x, y, TILE_BRICK)
+    end
+end
 -- Updates the values of a map
 function Map:update(dt)
  
@@ -84,13 +155,10 @@ end
 function Map:render()
     for y = 1, self.mapHeight do
         for x = 1, self.mapWidth do
-            if self:getTile(x, y) == TILE_BRICK then
-                -- draws out all of the values onto the map
-                love.graphics.draw(self.spritesheet, self.tileSprites[self:getTile(x, y)],
-                    (x - 1) * self.tileWidth, (y - 1) * self.tileHeight)
-            end
+            local tile = self:getTile(x, y)
+            love.graphics.draw(self.spritesheet, self.tileSprites[tile],
+                (x - 1) * self.tileWidth, (y - 1) * self.tileHeight)
         end
     end
 end
 
-return Map
--- mario/main.lua
+++ mario/main.lua
@@ -10,14 +10,18 @@ push = require 'push'
 require 'Util'
 
 require 'Map'
+
+-- Changes the filter that it will use so that when changing the size it is clear
+love.graphics.setDefaultFilter('nearest', 'nearest')
+
+math.randomseed(os.time())
+
+-- Assigning map to class Map
+map = Map()
+
+
 -- Loads out our screen and assigns objects to a class
 function love.load()
-
-    -- Assigning map to class Map
-    map = Map()
-
-    -- Changes the filter that it will use so that when changing the size it is clear
-    love.graphics.setDefaultFilter('nearest', 'nearest')
 
     -- Making the screen for our game
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
